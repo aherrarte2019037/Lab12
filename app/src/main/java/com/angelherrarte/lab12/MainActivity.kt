57637acb25dc7510c9d513832337f5936de4f353
Angel <angelherrarte3@gmail.com>
2023-11-07 09:19:16 -0600

    Se añadio imagen

--- app/src/main/java/com/angelherrarte/lab12/MainActivity.kt
+++ app/src/main/java/com/angelherrarte/lab12/MainActivity.kt
@@ -1,27 +1,38 @@
 package com.angelherrarte.lab12
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.angelherrarte.lab12.ui.theme.Lab12Theme
 
@@ -42,53 +53,64 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyResponsiveUI() {
-    // Determina la orientación de la pantalla
-    val orientation = LocalConfiguration.current.orientation
+    val scrollState = rememberScrollState()
+    val configuration = LocalConfiguration.current
+    val screenWidth = LocalDensity.current.run { configuration.screenWidthDp.dp }
+    val screenHeight = LocalDensity.current.run { configuration.screenHeightDp.dp }
+    val orientation = configuration.orientation
 
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        // Campos de texto
-        var textFieldValue = remember { TextFieldValue("") }
-        OutlinedTextField(
-            value = textFieldValue,
-            onValueChange = { newValue -> textFieldValue = newValue },
+    val columnModifier = if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+    } else {
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    }
+
+    Column(
+        modifier = columnModifier,
+        horizontalAlignment = if (screenWidth < 600.dp) Alignment.CenterHorizontally else Alignment.Start
+    ) {
+        TextField(
+            value = "",
+            onValueChange = {},
             label = { Text("Escribe algo") },
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
+            modifier = Modifier.fillMaxWidth()
         )
-
-        // Botón
+        Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = {},
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
-            Text("Presiona aquí")
+            Text("Botón")
         }
-
-        // Imagen
+        Spacer(modifier = Modifier.height(16.dp))
         Image(
-            painter = painterResource(id = R.drawable.my_image),
-            contentDescription = "Descripción de la imagen",
+            painter = painterResource(id = R.drawable.example_image),
+            contentDescription = "Imagen de ejemplo",
             contentScale = ContentScale.Crop,
-            modifier = Modifier.size(200.dp)
+            modifier = Modifier.size(80.dp).align(Alignment.CenterHorizontally)
         )
-
-        // Lista o Grid dependiendo de la orientación
+        Spacer(modifier = Modifier.height(16.dp))
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            // Lista vertical
             LazyColumn {
-                items(items = listOf("Item 1", "Item 2", "Item 3")) { item ->
-                    ListItem(item = item)
+                items(20) { index ->
+                    ListItemView("Item $index")
                 }
             }
         } else {
-            // Grid horizontal
-            LazyGrid(
+            LazyVerticalGrid(
                 columns = GridCells.Fixed(3),
-                contentPadding = PaddingValues(8.dp)
+                contentPadding = PaddingValues(8.dp),
+                modifier = Modifier.fillMaxHeight()
             ) {
-                items(items = listOf("Item 1", "Item 2", "Item 3")) { item ->
-                    ListItem(item = item)
+                items(20) { index ->
+                    ListItemView("Grid Item $index")
                 }
             }
         }
@@ -96,17 +118,14 @@ fun MyResponsiveUI() {
 }
 
 @Composable
-fun ListItem(item: String) {
-    Card(modifier = Modifier.padding(8.dp).fillMaxWidth(), elevation = 2.dp) {
-        Text(
-            text = item,
-            modifier = Modifier.padding(16.dp)
-        )
+fun ListItemView(text: String) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+    ) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Text(text = text)
+        }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyResponsiveUI()
 }
